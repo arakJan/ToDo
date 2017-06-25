9dc40b66629ee78216c98f825407c52819df5875
unknown <hmayakar@gmail.com>
2017-06-25 16:51:49 +0500

    adding functionality to edit and delete tasks

--- js/site.js
+++ js/site.js
@@ -2,5 +2,46 @@
  * Created by Admin on 6/23/2017.
  */
 $(document).ready(function () {
-    
+    $('input[type=checkbox]').change(function () {
+        if(this.checked){
+           var html = $(this).parent().html();
+            $(this).parent().remove();
+            $('#completed-tasks').append('<li>' + html +'</li>')
+        }else{
+            var html = $(this).parent().html();
+            $(this).parent().remove();
+            $('#incomplete-tasks').append('<li>' + html +'</li>')
+        }
+    })
 })
+
+var edit = document.getElementsByClassName('edit');
+var deleted = document.getElementsByClassName('delete');
+var checked = document.getElementsByClassName('in');
+
+
+for(var i=0;i<edit.length;i++){
+    edit[i].addEventListener('click',editContent);
+}
+
+for(var i=0;i<deleted.length;i++){
+    deleted[i].addEventListener('click',deleteTask);
+}
+
+
+function editContent() {
+    var parent = this.parentNode;
+    var textElement = parent.querySelector('p');
+    textElement.setAttribute("contenteditable", true);
+    textElement.focus();
+}
+
+function deleteTask() {
+    var parent = this.parentNode;
+    parent.remove();
+}
+
+
+
+
+
