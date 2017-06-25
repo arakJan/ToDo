e7055916536f7c280010dad78225938a071e579f
unknown <hmayakar@gmail.com>
2017-06-26 04:29:02 +0500

    design fixes + added functionality to auto-save in localstorage whenever something changes

--- js/site.js
+++ js/site.js
@@ -1,36 +1,67 @@
 /**
  * Created by Admin on 6/23/2017.
  */
+window.onload = function () {
+    var done = document.getElementById('done');
+    var todo = document.getElementById('todo');
+    typeof localStorage.done!='undefined' ? done.innerHTML = localStorage.done : done.innerHTML ='';
+    typeof localStorage.todo!='undefined' ? todo.innerHTML = localStorage.todo : todo.innerHTML ='';
+}
 document.body.addEventListener('click', function(event) {
     var parent = event.target.parentNode;
-    if(event.target.classList[0]== 'edit'){
+
+    if(hasClass(event.target,'edit')){
         var textElement = parent.querySelector('p');
         textElement.setAttribute("contenteditable", true);
         textElement.focus();
     };
-    if (event.target.classList[0] === 'delete') {
+    if (hasClass(event.target,'delete')) {
         parent.remove();
+        saveStorage();
     }
     if (event.target.type == 'checkbox') {
         moveTask(event.target.checked,event.target.parentNode);
     }
     if(event.target.id == 'new'){
-        console.log(event.target);
-        var taskList = document.getElementById('incomplete-tasks');
+        if(parent.querySelector('input').value.replace(/ /g,'') == ''){
+            alert('task can not be blank');
+            return false;
+        }
+        var taskList = document.getElementById('todo');
         var task = parent.querySelector('input').value;
+        parent.querySelector('input').value = '';
         var li = document.createElement('li');
-        li.innerHTML =  '<input type="checkbox" ><p><em>'+ task+'</em></p><button class="edit">Edit</button><button class="delete">Delete</button>'
-        taskList.appendChild(li)
+        li.innerHTML =  '<input type="checkbox" ><p class="taskText"><strong>'+ task+'</strong></p><button class="edit btn" title="Edit" class="edit"><i class="fa fa-pencil-square-o"></i></button>' +
+            '<button class=" delete btn" title="Delete" class="delete"><i class="fa fa-trash-o"></i></button>'
+        taskList.appendChild(li);
+        saveStorage();
     }
+});
+
+document.body.addEventListener('focusout', function(event) {
+    if(hasClass(event.target,'taskText') && event.target.contentEditable == 'true'){
+        event.target.contentEditable = 'false';
+        saveStorage()
+    };
 });
 
 function moveTask(checked,task) {
     if(checked == true){
-        var completed = document.getElementById('completed-tasks');
-        completed.appendChild(task)
+        var done = document.getElementById('done');
+        done.appendChild(task);
+        saveStorage()
     } else{
-        var incomplete = document.getElementById('incomplete-tasks');
-        incomplete.appendChild(task)
+        var todo = document.getElementById('todo');
+        todo.appendChild(task)
+        saveStorage()
     }
 }
-
+function hasClass(elem, className) {
+    return elem.className.split(' ').indexOf(className) > -1;
+}
+function saveStorage() {
+    var done = document.getElementById('done');
+    var todo = document.getElementById('todo');
+    localStorage.done = done.innerHTML;
+    localStorage.todo = todo.innerHTML;
+}
